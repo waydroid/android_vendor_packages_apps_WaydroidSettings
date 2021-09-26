eaf21166c406f349a907c36bc23605d806f9b763
tejHackerDEV <tejasimha222@gmail.com>
2021-09-26 23:30:12 +0530

    feat: Add WayDroid props
    Add persist.waydroid.multi_windows
    Add persist.waydroid.invert_colors
    Add persist.waydroid.height_padding
    Add persist.waydroid.width_padding
    Add waydroid.display_width
    Add persist.waydroid.width
    Add persist.waydroid.suspend
    
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- app/src/main/java/com/waydroid/settings/fragments/SettingsFragment.kt
+++ app/src/main/java/com/waydroid/settings/fragments/SettingsFragment.kt
@@ -9,45 +9,51 @@ import android.os.Bundle
 import android.util.Log
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SeekBarPreference
 import androidx.preference.SwitchPreferenceCompat
 import com.waydroid.settings.R
 import java.lang.reflect.InvocationTargetException
 
 class SettingsFragment : PreferenceFragmentCompat() {
-    private lateinit var switchEnablePCMode: SwitchPreferenceCompat
-    private lateinit var switchEnabledWayDroidSystemUI: SwitchPreferenceCompat
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
-        switchEnablePCMode = findPreference<Preference>(
-            getString(R.string.key_switch_enable_pc_mode)
-        ) as SwitchPreferenceCompat
-        switchEnablePCMode.isChecked = getBooleanSystemProperties(PROPERTY_PC_MODE_KEY)
-        switchEnabledWayDroidSystemUI = findPreference<Preference>(
-            getString(R.string.key_switch_enable_bd_nav_bar)
-        ) as SwitchPreferenceCompat
-        switchEnabledWayDroidSystemUI.isChecked =
-            getBooleanSystemProperties(PROPERTY_BD_SYSTEMUI_KEY)
-        switchEnablePCMode.onPreferenceClickListener =
-            Preference.OnPreferenceClickListener { preference: Preference ->
-                enablePCMode((preference as SwitchPreferenceCompat).isChecked)
-                true
-            }
-        switchEnabledWayDroidSystemUI.onPreferenceClickListener =
-            Preference.OnPreferenceClickListener { preference: Preference ->
-                enableWayDroidSystemUI((preference as SwitchPreferenceCompat).isChecked)
-                true
-            }
+
+        switch(findPreference<Preference>(getString(R.string.key_switch_multi_windows_mode)) as SwitchPreferenceCompat, PROPERTY_WD_MULTI_WINDOWS_KEY)
+        switch(findPreference<Preference>(getString(R.string.key_switch_invert_colors)) as SwitchPreferenceCompat, PROPERTY_WD_INVERT_COLORS_KEY)
+        switch(findPreference<Preference>(getString(R.string.key_switch_enable_never_sleep)) as SwitchPreferenceCompat, PROPERTY_WD_NEVER_SLEEP_KEY)
+
+        seekBar(findPreference<Preference>(getString(R.string.key_seekbar_height_padding)) as SeekBarPreference, PROPERTY_WD_HEIGHT_PADDING_KEY)
+        seekBar(findPreference<Preference>(getString(R.string.key_seekbar_width_padding)) as SeekBarPreference, PROPERTY_WD_WIDTH_PADDING_KEY)
+        seekBar(findPreference<Preference>(getString(R.string.key_seekbar_display_width)) as SeekBarPreference, PROPERTY_WD_DISPLAY_WIDTH_KEY)
+        seekBar(findPreference<Preference>(getString(R.string.key_seekbar_wd_width)) as SeekBarPreference, PROPERTY_WD_WIDTH_KEY)
     }
 
-    private fun enablePCMode(enable: Boolean) {
-        Log.d(TAG, "enable pc mode $enable")
-        setBooleanSystemProperties(PROPERTY_PC_MODE_KEY, enable)
+    private fun switch(switchPreferenceCompat: SwitchPreferenceCompat, property: String) {
+        switchPreferenceCompat.apply {
+            isChecked = getBooleanSystemProperties(property)
+            onPreferenceClickListener =
+                Preference.OnPreferenceClickListener { preference: Preference ->
+                    setProperty(property, (preference as SwitchPreferenceCompat).isChecked.toString())
+                    true
+                }
+        }
+    }
+
+    private fun seekBar(seekBarPreference: SeekBarPreference, property: String) {
+        seekBarPreference.apply {
+            value = getIntSystemProperties(property)
+            onPreferenceChangeListener =
+                Preference.OnPreferenceChangeListener { _, newValue ->
+                    setProperty(property, newValue.toString())
+                    true
+                }
+        }
     }
 
     private fun enableWayDroidSystemUI(enable: Boolean) {
         Log.d(TAG, "enable WayDroid SystemUI $enable")
-        setBooleanSystemProperties(PROPERTY_BD_SYSTEMUI_KEY, enable)
+        setSystemProperties(PROPERTY_BD_SYSTEMUI_KEY, enable.toString())
         val context: Context? = activity
         if (context != null) {
             val packageName = context.packageName
@@ -62,11 +68,16 @@ class SettingsFragment : PreferenceFragmentCompat() {
         }
     }
 
-    private fun setBooleanSystemProperties(key: String, value: Boolean) {
+    private fun setProperty(mode: String, value: String) {
+        Log.d(TAG, "$mode $value")
+        setSystemProperties(mode, value)
+    }
+
+    private fun setSystemProperties(key: String, value: String) {
         try {
             @SuppressLint("PrivateApi") val clazz = Class.forName(SYSTEM_PROPERTIES_CLASS_NAME)
             val setMethod = clazz.getMethod("set", String::class.java, String::class.java)
-            setMethod.invoke(null, key, if (value) "true" else "false")
+            setMethod.invoke(null, key, value)
         } catch (e: ClassNotFoundException) {
             Log.d(TAG, "Failed to set value $value for $key")
         } catch (e: NoSuchMethodException) {
@@ -99,10 +110,37 @@ class SettingsFragment : PreferenceFragmentCompat() {
         return true
     }
 
+    private fun getIntSystemProperties(key: String): Int {
+        try {
+            @SuppressLint("PrivateApi")
+            val clazz = Class.forName(SYSTEM_PROPERTIES_CLASS_NAME)
+            val setMethod = clazz.getMethod(
+                "getInt",
+                String::class.java, Int::class.javaPrimitiveType
+            )
+            return setMethod.invoke(null, key, 0) as Int
+        } catch (e: ClassNotFoundException) {
+            Log.d(TAG, "Failed to get value for $key")
+        } catch (e: NoSuchMethodException) {
+            Log.d(TAG, "Failed to get value for $key")
+        } catch (e: IllegalAccessException) {
+            Log.d(TAG, "Failed to get value for $key")
+        } catch (e: InvocationTargetException) {
+            Log.d(TAG, "Failed to get value for $key")
+        }
+        return 0
+    }
+
     companion object {
-        private const val TAG = "BDSettingsFragment"
+        private const val TAG = "WDSettingsFragment"
         private const val SYSTEM_PROPERTIES_CLASS_NAME = "android.os.SystemProperties"
-        private const val PROPERTY_PC_MODE_KEY = "persist.sys.pcmode.enabled"
         private const val PROPERTY_BD_SYSTEMUI_KEY = "persist.sys.systemuiplugin.enabled"
+        private const val PROPERTY_WD_MULTI_WINDOWS_KEY = "persist.waydroid.multi_windows"
+        private const val PROPERTY_WD_INVERT_COLORS_KEY = "persist.waydroid.invert_colors"
+        private const val PROPERTY_WD_HEIGHT_PADDING_KEY = "persist.waydroid.height_padding"
+        private const val PROPERTY_WD_WIDTH_PADDING_KEY = "persist.waydroid.width_padding"
+        private const val PROPERTY_WD_DISPLAY_WIDTH_KEY = "waydroid.display_width"
+        private const val PROPERTY_WD_WIDTH_KEY = "persist.waydroid.width"
+        private const val PROPERTY_WD_NEVER_SLEEP_KEY = "persist.waydroid.suspend"
     }
 }
